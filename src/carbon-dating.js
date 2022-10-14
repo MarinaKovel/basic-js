901e21e00d727fec613d0ebc63487b86e5bb927c
Marina Kovel <waveee@gmail.com>
2022-10-14 13:06:24 +0300

    feat: complete task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let result = 0;
+  if ((typeof sampleActivity === "string") && (+sampleActivity > 0) && (+sampleActivity > 0) && (+sampleActivity < 15)) {
+    let N0toN = MODERN_ACTIVITY / +sampleActivity;
+  let k = (0.693 * 1000) / (HALF_LIFE_PERIOD * 1000);
+  let t = (Math.log(N0toN) * 1e16) / (k * 1e16);
+  result = Math.ceil(t);
+  } else {
+    result = false;
+  } 
+  return result;
 }
 
 module.exports = {
